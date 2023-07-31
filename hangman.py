eed08c8f2d626a680a174c4f99baad45cb001633
Shuxiang-Y <jiamingy1203@gmail.com>
2023-08-01 02:22:33 +0900

    1st commit

---
+++ hangman.py
@@ -0,0 +1,45 @@
+def hangman(word):
+    wrong = 0
+    stages = ["",
+              "______     ",
+              "|          ",
+              "|      |   ",
+              "|      0   ",
+              "|     /|\  ",
+              "|     / \  ",
+              "|          ",
+              ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False 
+    
+    print("Welcome to hangman!")
+    
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "guess a charactor."
+        char = input(msg)
+        
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = "$"
+            
+        else:
+            wrong += 1
+        
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        
+        if "_" not in board:
+            print("you win!")
+            print("_".join(board))
+            win = True
+            
+    if not win:
+            print("\n".join(stages[0:wrong+1]))
+            print("you lose! The word is {}".format(word))
+            
+            
+hangman("cat")
